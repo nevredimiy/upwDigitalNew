35a93f281f92f1ba9100a76dc51db8d221941f19
unknown <artemlitvinov81@gmail.com>
2023-01-22 23:03:53 +0300

    comb the hair2

--- build/js/script.js
+++ build/js/script.js
@@ -2,17 +2,17 @@ const burger = document.getElementById('burger');
 const menu = document.getElementById('header_menu');
 
 const initApp = () => {
-    const hamburgerBtn = document.getElementById('hamburger-button')
-    const mobileMenu = document.getElementById('mobile-menu')
+  const hamburgerBtn = document.getElementById('hamburger-button')
+  const mobileMenu = document.getElementById('mobile-menu')
 
-    const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden')
-        mobileMenu.classList.toggle('flex')
-        hamburgerBtn.classList.toggle('toggle-btn')
-    }
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle('hidden')
+    mobileMenu.classList.toggle('flex')
+    hamburgerBtn.classList.toggle('toggle-btn')
+  }
 
-    hamburgerBtn.addEventListener('click', toggleMenu)
-    mobileMenu.addEventListener('click', toggleMenu)
+  hamburgerBtn.addEventListener('click', toggleMenu)
+  //mobileMenu.addEventListener('click', toggleMenu)
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
@@ -22,7 +22,7 @@ var plus = document.getElementsByClassName("plus-minus");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.children[0].nextElementSibling.classList.toggle("toggle-plus");
